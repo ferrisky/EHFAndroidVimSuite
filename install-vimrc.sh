da05771e8cb6e9a3bc407998517f8b1eec5f3b23
Ferrisky <Ferrisky@FIH>
2015-02-06 16:21:23 +0800

    install shell script refinement with backup mechanism

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+#back up original first
+mv ~/.vimrc ~/vimrc.bak
+mv ~/.vim ~/.vim.bak
+
+#remove existed one
+rm -rf ~/.vimrc
+rm -rf ~/.vim
+
+#copy the new one
 cp -f ./vim_dir/vimrc ~/.vimrc
 cp -Rf ./vim_dir ~/.vim
 #ln -s .vim/gvimrc .gvimrc
