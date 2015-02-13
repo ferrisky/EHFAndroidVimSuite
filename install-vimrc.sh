b6c4f31a2cbd70eb73d92126c3b02f96b597b9f4
Ferrisky <Ferrisky@FIH>
2015-02-13 12:31:17 +0800

    fixed power line

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 #back up original first
+rm -rf  ~/vimrc.bak
+rm -rf ~/.vim.bak
 mv ~/.vimrc ~/vimrc.bak
 mv ~/.vim ~/.vim.bak
 
 #remove existed one
 rm -rf ~/.vimrc
 rm -rf ~/.vim
-
+rm -rf /tmp/PowerLine.cache
 #copy the new one
 cp -f ./vim_dir/vimrc ~/.vimrc
 cp -Rf ./vim_dir ~/.vim
